8fb2a1b32c926974c5ddf28fe0e473cd96285535
AndrickSilva <silva.andrick2001@gmail.com>
2022-08-11 15:47:49 +0530

    added scroll

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// scroll button
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 50); 
+        return false; 
+    }); 
+ });
