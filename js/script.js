31772359186a67beeed75421673dce1f69d4fa4e
AndrickSilva <silva.andrick2001@gmail.com>
2022-08-13 15:41:59 +0530

    added scrolltop  and nav toggle

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-// scroll button
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 50); 
-        return false; 
-    }); 
- });
+
+// navigation-toggle
+
+var bars = document.querySelector(".bars i");
+var header = document.querySelector(".header");
+
+bars.addEventListener('click', function () {
+    header.classList.toggle('active')
+    // bars.classList.toggle("hide");
+    if (bars.className == "fa-solid fa-bars") {
+
+        bars.className = 'fa-solid fa-xmark'
+    } else {
+        bars.className = 'fa-solid fa-bars'
+    }
+});
+
+
+// scroll
+
+window.addEventListener('scroll', function () {
+    const scroll = document.querySelector('.scrollTop');
+    scroll.classList.toggle("active", window.scrollY > 400)
+})
+
+
+
