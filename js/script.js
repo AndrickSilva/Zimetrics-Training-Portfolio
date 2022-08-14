16355903562e476ba709133c0db77e5eca5af64b
AndrickSilva <silva.andrick2001@gmail.com>
2022-08-14 13:51:28 +0530

    ternary operator & active link half logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // navigation-toggle
 
 var bars = document.querySelector(".bars i");
@@ -6,13 +5,7 @@ var header = document.querySelector(".header");
 
 bars.addEventListener('click', function () {
     header.classList.toggle('active')
-    // bars.classList.toggle("hide");
-    if (bars.className == "fa-solid fa-bars") {
-
-        bars.className = 'fa-solid fa-xmark'
-    } else {
-        bars.className = 'fa-solid fa-bars'
-    }
+    (bars.className == "fa-solid fa-bars" ? bars.className = 'fa-solid fa-xmark' : bars.className = 'fa-solid fa-bars')
 });
 
 
@@ -24,4 +17,28 @@ window.addEventListener('scroll', function () {
 })
 
 
+// active links on scroll (Incomplete logic)
 
+const sec = document.querySelectorAll('section');
+const li = document.querySelectorAll('.list-group-item');
+
+window.addEventListener('scroll', function(){
+    let len = sec.length;
+    while(--len && window.scrollY + 500 < sec[len].offsetTop){}
+    li.forEach(ltx => ltx.classList.remove('active'));
+    li[len].classList.add('active');
+});
+
+
+
+
+// slider
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
