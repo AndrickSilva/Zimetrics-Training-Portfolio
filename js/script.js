f9ebb3cd9bc1264a96e3e97ed4f7d835a22404b7
AndrickSilva <silva.andrick2001@gmail.com>
2022-08-17 12:36:34 +0530

    added swiper

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var header = document.querySelector(".header");
 
 bars.addEventListener('click', function () {
     header.classList.toggle('active')
-    (bars.className == "fa-solid fa-bars" ? bars.className = 'fa-solid fa-xmark' : bars.className = 'fa-solid fa-bars')
+        (bars.className == "fa-solid fa-bars" ? bars.className = 'fa-solid fa-xmark' : bars.className = 'fa-solid fa-bars')
 });
 
 
@@ -22,14 +22,31 @@ window.addEventListener('scroll', function () {
 const sec = document.querySelectorAll('section');
 const li = document.querySelectorAll('.list-group-item');
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     let len = sec.length;
-    while(--len && window.scrollY + 500 < sec[len].offsetTop){}
+    while (--len && window.scrollY + 500 < sec[len].offsetTop) { }
     li.forEach(ltx => ltx.classList.remove('active'));
     li[len].classList.add('active');
 });
 
+//typeWriter
 
+var app = document.getElementById('typeWriterText');
+
+var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+});
+
+typewriter
+    .pauseFor(2500)
+    .typeString(' Im a <span class="typeUnderline"> Developer </span> ')
+    .pauseFor(300)
+    .deleteChars(13)
+    .typeString('nd <span class="typeUnderline"> Designer </span>')
+    // .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+    .pauseFor(1000)
+    .start();
 
 
 // slider
